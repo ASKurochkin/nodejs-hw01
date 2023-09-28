e4c06d4155a945ffbe6fd0040aea8ce889ec237b
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-09-28 18:42:09 +0300

    Add Node.js HW-01

---
+++ README.md
@@ -0,0 +1,4 @@
+1. action-list https://monosnap.com/file/8QyGDGeLLv9f81yl5n44BkGEQkuVnO
+2. action-get https://monosnap.com/file/F107xTM2yncCHLcwgYSaOBX70jJjbF
+3. action-add https://monosnap.com/file/nUPTQTXcYjThelIoN1JmXJ0XIpH3QL
+4. action-remove https://monosnap.com/file/P9o2ZQOqDqezhEknf6KQnmuazve8n5
---
+++ contacts.js
@@ -0,0 +1,40 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+async function listContacts() {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const ourContact = contacts.find((contact) => contact.id === contactId);
+  return ourContact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const remContact = await getContactById(contactId);
+  if (remContact) {
+    const contactIndex = contacts.indexOf(remContact);
+    contacts.splice(contactIndex, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return remContact;
+  }
+  return null;
+}
+
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), ...data };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts, getContactById, removeContact, addContact
+}
